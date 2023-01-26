9bcb42d854c92143f1c11a50a02c82e57d66b467
Moutamid <lumianokia188@gmail.com>
2023-01-26 21:57:39 +0500

    initial commit

--- app/src/main/java/com/moutamid/demovideoplayer/SplashActivity.java
+++ app/src/main/java/com/moutamid/demovideoplayer/SplashActivity.java
@@ -3,9 +3,20 @@ package com.moutamid.demovideoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,8 +27,38 @@ public class SplashActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         findViewById(R.id.imagevieww).setOnClickListener(v -> {
-            finish();
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            Dexter.withActivity(SplashActivity.this)
+                    .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .withListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted(PermissionGrantedResponse response) {
+                            finish();
+                            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        }
+
+                        @Override
+                        public void onPermissionDenied(PermissionDeniedResponse response) {
+                            if (response.isPermanentlyDenied()) {
+                                // open device settings when the permission is
+                                // denied permanently
+                                Toast.makeText(SplashActivity.this, "You need to provide permission!", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent();
+                                intent.setAction(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package",
+                                        BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                            token.continuePermissionRequest();
+                        }
+                    }).check();
         });
 
     }
