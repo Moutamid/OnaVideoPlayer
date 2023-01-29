7e014f2f02e5a1d08c8aba3ed8f6d6bdf7e5646e
Moutamid <lumianokia188@gmail.com>
2023-01-29 15:21:21 +0500

    initial commit

--- app/src/main/java/com/moutamid/demovideoplayer/VideoPlayerActivity.java
+++ app/src/main/java/com/moutamid/demovideoplayer/VideoPlayerActivity.java
@@ -9,11 +9,14 @@ import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import com.khizar1556.mkvideoplayer.MKPlayer;
+import com.khizar1556.mkvideoplayer.MKPlayerActivity;
 import com.moutamid.demovideoplayer.databinding.ActivityVideoPlayerBinding;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
     private ActivityVideoPlayerBinding b;
+    MKPlayer mkplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +25,34 @@ public class VideoPlayerActivity extends AppCompatActivity {
         b = ActivityVideoPlayerBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
-        /*Uri uri = Uri.parse(getIntent().getStringExtra("path"));
-        b.videoView.setVideoURI(uri);*/
-
-        b.videoView.setMediaController(new MediaController(this));
+       /* b.videoView.setMediaController(new MediaController(this));
         b.videoView.setVideoPath(getIntent().getStringExtra("path"));
-        b.videoView.start();
+        b.videoView.start();*/
 
+//        MKPlayerActivity.configPlayer(this).play(getIntent().getStringExtra("path"));
+
+        mkplayer = new MKPlayer(this);
+        mkplayer.play(getIntent().getStringExtra("path"));
+
+        mkplayer.setPlayerCallbacks(new MKPlayer.playerCallbacks() {
+            @Override
+            public void onNextClick() {
+                mkplayer.forward(0.1f);
+            }
+
+            @Override
+            public void onPreviousClick() {
+                mkplayer.forward(-0.1f);
+            }
+        });
+
+        mkplayer.setShowNavIcon(false);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        mkplayer.stop();
+        super.onBackPressed();
     }
 }
